581e8ac2d29168bd44af55a8a469058c9249bc61
matsuken8000 <nmk100523091452050750@gmail.com>
2021-01-01 22:08:40 +0000

    Finish login-function

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,22 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  
+  # セッションと@current_userを破棄します
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+  #現在ログイン中のユーザーがいる場合オブジェクトを返します。
+  def current_user
+    if session[:user_id]
+      current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+  
+  #現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
+  def logged_in?
+    !current_user.nil?
+  end
 end
