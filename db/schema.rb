d0cbfa2173eb0491530ebc6a5a8b3a3b1505681d
matsuken8000 <nmk100523091452050750@gmail.com>
2021-01-12 22:00:04 +0000

    10章10.4終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210109212714) do
+ActiveRecord::Schema.define(version: 20210111020544) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.date "worked_on"
+    t.datetime "started_at"
+    t.datetime "finished_at"
+    t.string "note"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -25,5 +36,4 @@ ActiveRecord::Schema.define(version: 20210109212714) do
     t.datetime "work_time", default: "2021-01-09 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
